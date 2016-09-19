e5660650004f77df0388e6a6b7b146eb486e7b2b
Yassi <yassih84@gmail.com>
2016-09-19 06:57:10 -0400

    All My App is done under lesson 01

--- lessons/01-setting-up/index.js
+++ lessons/01-setting-up/index.js
@@ -1,4 +1,12 @@
+
+
 import React from 'react'
 import { render } from 'react-dom'
-import App from './modules/App'
-render(<App/>, document.getElementById('app'))
+import { Router, browserHistory } from 'react-router'
+// import routes and pass them into <Router/>
+import routes from './modules/routes'
+
+render(
+  <Router routes={routes} history={browserHistory}/>,
+  document.getElementById('app')
+)
--- lessons/01-setting-up/modules/App.js
+++ lessons/01-setting-up/modules/App.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router'
+import { IndexLink } from 'react-router'
+import NavLink from './NavLink.js'
+import Home from './Home'
 
 export default React.createClass({
+
+
   render() {
-    return <div>Hello, React Router!</div>
+    return (
+      <div>
+        {this.props.children || <Home/>}
+        <h1>React Router Tutorial</h1>
+        <ul role="nav">
+        <NavLink/>
+          <li><NavLink to="/" onlyActiveOnIndex>Home</NavLink></li>
+          <li><NavLink to="/about" activeClassName="active">About</NavLink></li>
+          <li><NavLink to="/repos" activeClassName="active">Repos</NavLink></li>
+        </ul>
+          {this.props.children}
+      </div>
+    )
   }
+
 })
